e48e410eea55f6a605b51d7f8039980e5a196410
Ilkka Kiistala <ilkka.kiistala@gmail.com>
2014-06-23 13:12:37 +0300

    ready to serve

--- README.md
+++ README.md
@@ -1,3 +1,52 @@
 Course Project
 ============================================
-This is a course project.
+This is a course project for Getting and cleaning data course in Coursera.
+
+Goal was to write a script that creates a tidy dataset [1].
+
+Information about the dataset [2]: 
+http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones 
+
+run_analysis.R
+--------------
+ 
+The script merges the training and test sets to a one data set.
+
+It then extracts the measurements that are mean and standard deviations of each measurement.
+
+The column names were added to describe the measured variables. 
+
+Finally a second, independent tidy data set was created. It includes the average of each selected variable for each activity and each subject. 
+
+The activity codes were changed into names that describe the activities in the data set (e.g. "standing").
+
+
+Data used:
+----------------
+
+- 'features_info.txt': Shows information about the variables used on the feature vector.
+
+- 'features.txt': List of all features.
+
+- 'activity_labels.txt': Links the class labels with their activity name.
+
+- 'train/X_train.txt': Training set.
+
+- 'train/y_train.txt': Training labels.
+
+- 'test/X_test.txt': Test set.
+
+- 'test/y_test.txt': Test labels.
+
+
+The following files were available for the train and test data. Their descriptions are equivalent. 
+
+- 'train/subject_train.txt': Each row identifies the subject who performed the activity for each window sample. Its range is from 1 to 30. 
+
+
+References:
+--------------------
+
+[1] Wicham, H. (under review?). Tidy Data. Journal of Statistical Software.
+
+[2] Davide Anguita, Alessandro Ghio, Luca Oneto, Xavier Parra and Jorge L. Reyes-Ortiz. Human Activity Recognition on Smartphones using a Multiclass Hardware-Friendly Support Vector Machine. International Workshop of Ambient Assisted Living (IWAAL 2012). Vitoria-Gasteiz, Spain. Dec 2012
--- run_analysis.R
+++ run_analysis.R
@@ -1,60 +1,104 @@
-# You should create one R script called run_analysis.R that does the following. 
-# 
-# 1 Merges the training and the test sets to create one data set.
-# 2 Extracts only the measurements on the mean and standard deviation for each measurement. 
-# 3 Uses descriptive activity names to name the activities in the data set
-# 4 Appropriately labels the data set with descriptive variable names. 
-# 5 Creates a second, independent tidy data set with the average of each variable for each activity and each subject. 
-# 
+# Ilkka Kiistala
+# Coursera: Getting and Cleaning Data
+# 2014-06-23
 
-# 1
+# empty workspace
+rm(list = ls(all = TRUE))
 
+# read files:
+
+# training data:
 dir <- "UCI HAR Dataset/train/"
 group <- "train"
-
 df <- read.table( paste0(dir, "subject_", group, ".txt") )
 df <- cbind(df, read.table( paste0(dir, "y_", group, ".txt") ))
 df <- cbind(df, read.table( paste0(dir, "X_", group, ".txt") ))
-
-df <- cbind(df, read.table( paste0(dir, "Inertial Signals/body_acc_x_", group, ".txt") ))
-df <- cbind(df, read.table( paste0(dir, "Inertial Signals/body_acc_y_", group, ".txt") ))
-df <- cbind(df, read.table( paste0(dir, "Inertial Signals/body_acc_z_", group, ".txt") ))
-
-df <- cbind(df, read.table( paste0(dir, "Inertial Signals/body_gyro_x_", group, ".txt") ))
-df <- cbind(df, read.table( paste0(dir, "Inertial Signals/body_gyro_x_", group, ".txt") ))
-df <- cbind(df, read.table( paste0(dir, "Inertial Signals/body_gyro_x_", group, ".txt") ))
-
-df <- cbind(df, read.table( paste0(dir, "Inertial Signals/total_acc_x_", group, ".txt") ))
-df <- cbind(df, read.table( paste0(dir, "Inertial Signals/total_acc_x_", group, ".txt") ))
-df <- cbind(df, read.table( paste0(dir, "Inertial Signals/total_acc_x_", group, ".txt") ))
-
 trainingData <- df
 
-#
-
+# testing data:
 dir <- "UCI HAR Dataset/test/"
 group <- "test"
-
 df <- read.table( paste0(dir, "subject_", group, ".txt") )
 df <- cbind(df, read.table( paste0(dir, "y_", group, ".txt") ))
 df <- cbind(df, read.table( paste0(dir, "X_", group, ".txt") ))
-
-df <- cbind(df, read.table( paste0(dir, "Inertial Signals/body_acc_x_", group, ".txt") ))
-df <- cbind(df, read.table( paste0(dir, "Inertial Signals/body_acc_y_", group, ".txt") ))
-df <- cbind(df, read.table( paste0(dir, "Inertial Signals/body_acc_z_", group, ".txt") ))
-
-df <- cbind(df, read.table( paste0(dir, "Inertial Signals/body_gyro_x_", group, ".txt") ))
-df <- cbind(df, read.table( paste0(dir, "Inertial Signals/body_gyro_x_", group, ".txt") ))
-df <- cbind(df, read.table( paste0(dir, "Inertial Signals/body_gyro_x_", group, ".txt") ))
-
-df <- cbind(df, read.table( paste0(dir, "Inertial Signals/total_acc_x_", group, ".txt") ))
-df <- cbind(df, read.table( paste0(dir, "Inertial Signals/total_acc_x_", group, ".txt") ))
-df <- cbind(df, read.table( paste0(dir, "Inertial Signals/total_acc_x_", group, ".txt") ))
-
 testData <- df
 
+# combine:
 allData <- rbind(trainingData, testData)
 
-# 2 
+# add column names
+namesData <- read.table("UCI HAR Dataset/features.txt", stringsAsFactors=F)
+namesData <- namesData[,2]
+names(allData) <- c("subject", "activityCode", namesData)
+
+# these columns have "mean" or "std" in their name:
+meanOrStd <- grepl("mean|std", names(allData))
+
+# dont't forget these, though:
+meanOrStd[1:2] <- TRUE
+
+# select all rows, but only specific columns:
+selectedData <- allData[, meanOrStd]
 
 
+# Wanted result: 30 subjects x 6 activities = 180 rows
+# this will be our result data set:
+meanDF <- selectedData[NULL,]
+
+# the code above copies the columns names info meanDF, but somehow they get vanished in the double for loop
+
+# make a shortcut variable to reference selectedData:
+d <- selectedData
+
+# the double for loop
+for (s in sort(unique(d$subject)))
+{
+  # this will tell where we are going:
+  print (s)
+
+  for (a in sort(unique(d$activityCode)))
+  {
+    sel <- d[ d$subject==s & d$activityCode==a, ]
+
+    # this is why it's handy to keep the activity code column remain as numeric:
+    means <- colMeans(sel)
+
+    # combine previous "means rows" with this new one:
+    meanDF <- rbind(meanDF, means, deparse.level=0)
+  }
+}
+
+# if you want to check that the target combinations exist:
+# meanDF[,1:2]
+
+ncol(meanDF)
+# should return 81
+
+nrow(meanDF)
+# should return 180
+
+# names(meanDF)
+# copy names from d data.frame because they get written over in the rbind():
+names(meanDF) <- names(d)
+
+# re-value activity values last:
+actNames <- read.table("UCI HAR Dataset/activity_labels.txt")
+meanDF$activityCode <- actNames[meanDF$activityCode,2]
+
+# it's user-friendly to rename the column accordingly:
+colnames(meanDF)[2] <- "activity"
+
+names(meanDF)
+
+
+# with these snippets you can check the data is refined as it was supposed to:
+mean( selectedData[selectedData$subject==30 & selectedData$activityCode==6, "tBodyGyro-mean()-X"] )
+# [1] -0.02678122
+meanDF[ meanDF$subject==30 & meanDF$activity=="LAYING", "tBodyGyro-mean()-X"]
+# [1] -0.02678122
+
+# save as a file:
+write.table(meanDF, "means.txt")
+
+# this is a code snippet I used to write me a list of column names:
+for (name in names(meanDF)) { cat( sprintf("%s\n", name)) }
